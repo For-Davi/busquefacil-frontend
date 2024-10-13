fa773a334b70f354b06373a9832859b1fda05cd4
Carlos Davi <carlos@datasource.expert>
2024-10-13 00:48:59 -0300

    Feat: Alterado visual do scrollbar

---
+++ src/components/content/Offers.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'Offers',
+});
+</script>
+<template>
+  <div class="full-width bg-blue-grey-1">Ofertas</div>
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,16 +3,18 @@ import NavbarHome from 'src/components/headers/NavbarHome.vue';
 import NavbarFilter from 'src/components/headers/NavbarFilter.vue';
 import Banner from 'src/components/content/Banner.vue';
 import FiltersRound from 'src/components/content/FiltersRound.vue';
+import Offers from 'src/components/content/Offers.vue';
 
 defineOptions({
   name: 'Home',
 });
 </script>
 <template>
-  <div class="row items-center justify-evenly">
+  <div class="column items-center justify-center">
     <NavbarHome />
     <NavbarFilter />
     <Banner />
     <FiltersRound />
+    <Offers />
   </div>
 </template>
